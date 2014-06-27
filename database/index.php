7666c5a581be035edc3d2b3369db52d6eec7a66a
arhis77 <arhis77@hotmail.com>
2014-06-27 21:56:49 +0400

    Update index.php

--- database/index.php
+++ database/index.php
@@ -2,3 +2,4 @@ $pdo = new PDO(
 'mysql:host=localhost;dbname=polyclinic;charset=utf-8', 'root', '');
 $statement = $pdo->query("SELECT name FROM patients");
 $row = $statement->fetch(PDO::FETCH_ASSOC); echo htmlentities($row['name']);
+$conn->setAtribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
