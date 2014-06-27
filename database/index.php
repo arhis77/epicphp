73d5d3c40cb4f24f0fbc20933792800bd6ec3efa
arhis77 <arhis77@hotmail.com>
2014-06-27 22:00:53 +0400

    Update index.php

--- database/index.php
+++ database/index.php
@@ -2,4 +2,6 @@ $pdo = new PDO(
 'mysql:host=localhost;dbname=polyclinic;charset=utf-8', 'root', '');
 $statement = $pdo->query("SELECT name FROM patients");
 $row = $statement->fetch(PDO::FETCH_ASSOC); echo htmlentities($row['name']);
-$conn->setAtribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+try{$conn = new PDO('mysql:host=localhost;dbname=polyclinic', 'root', '');
+$conn->setAtribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);}
+catch(PDOException $e) {echo 'ERROR: ' . $e->getMessage();
